5e7185077c26286a9a32a41721046377d1f52618
Faizanahmed Saiyed <saiyed.faizanahmed1@gmail.com>
2023-08-29 15:28:50 +0530

    first commit

---
+++ RemoveDuplicate.java
@@ -0,0 +1,31 @@
+// A HashSet is a data structure in programming that stores a collection of unique elements, meaning it doesn't allow duplicate values. It offers fast and efficient operations for adding, removing, and checking the presence of elements.
+
+import java.util.HashSet;
+import java.util.Set;
+
+class RemoveDuplicate {
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,8};
+        RemoveDuplicate remover = new RemoveDuplicate();
+        boolean hasDuplicates = remover.containsDuplicate(nums);
+        System.out.println("Conatins Duplicates: " + hasDuplicates );
+
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+
+        for (int num : nums) {
+            if (numSet.contains(num)) {
+                return true; // Found a duplicate
+            }
+
+            numSet.add(num);
+        }
+
+        return false;
+
+    }
+
+}
